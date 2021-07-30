f386ea7368e0639ee8da6ac9f8fd009015bbf93b
karura195 <karla.diaz@pucp.pe>
2021-07-30 11:31:39 -0700

    Complex maths

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
  * display_complex_number - displays a complex number a + ib
@@ -8,25 +7,30 @@
  * Return: void
  */
 
-struct complex
-{
-  double re;
-  double im;
-};
-typedef struct complex complex;
-
 void display_complex_number(complex c)
 {
-    if (c.im > 0)
-    {
-      printf("%.0f + %.0fi\n", c.re, c.im);
-    }
-    else if (c.im == 0)
-    {
-      printf("%.0f\n", c.re);
-    }
-    else if (c.re == 0)
-    {
-      printf("%.0fi\n", c.im);
-    }
+	if (c.im > 1)
+	{
+		printf("%.0f + %.0fi\n", c.re, c.im);
+	}
+	else if (c.im == 0)
+	{
+		printf("%.0f\n", c.re);
+	}
+	else if (c.re == 0)
+	{
+		printf("%.0fi\n", c.im);
+	}
+	else if (c.im == 1)
+	{
+		printf("%.0f + i\n", c.re);
+	}
+	else if (c.im == -1)
+	{
+		printf("%.0f - i\n", c.re);
+	}
+	else if (c.im < -1)
+	{
+		printf("%.0f - %.0fi\n", c.re, (-c.im));
+	}
 }
